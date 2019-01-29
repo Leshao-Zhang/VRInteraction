61c3aa82f9bc4622dba035ea83ce33b09edaa510
Leshao Zhang <zhangleshao@gmail.com>
2019-01-29 23:39:12 +0000

    Add files via upload

---
+++ Gaze Interaction/Assets/Script/GazeSwitcher.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GazeSwitcher : MonoBehaviour
+{
+    // Start is called before the first frame update
+    RaycastHit hit;
+    int LayerMask = 1 << 9;
+    GameObject loading;
+    bool staring = false;
+    Image imageComp;
+    public float progressSpeed = 0.5f;
+    bool triggered = false;
+    //above do not change
+
+    //add objects below
+    Light mylight;
+    GameObject switcher;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loading=GameObject.Find("loading1");
+        loading.SetActive(false);
+        imageComp = loading.GetComponent<Image>();
+        imageComp.fillAmount = 0;
+        //above do not change
+
+        //initialize objects
+        mylight = GameObject.Find("Point Light").GetComponent<Light>();
+        switcher = GameObject.Find("Switcher");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity, LayerMask))
+        {
+            Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+            staring = true;        
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.forward * 1000, Color.white);
+            Debug.Log("Did not Hit");
+            staring = false;
+            imageComp.fillAmount = 0;
+            triggered = false;
+        }
+        if (staring && imageComp.fillAmount < 1)
+        {
+            imageComp.fillAmount += progressSpeed * Time.deltaTime;
+            loading.SetActive(true);
+        }
+        if (imageComp.fillAmount >= 1)
+        {
+            loading.SetActive(false);
+            if (!triggered)
+            {
+                //here we can set different rule for different object.
+                if (hit.transform.gameObject.Equals(switcher))
+                {
+                    mylight.enabled = !mylight.enabled;
+                }
+                /**
+                 * For example:
+                 * 
+                 * if (hit.transform.gameObject.Equals(someOtherSwitcher))
+                 * {
+                 *      Do something;
+                 * }    
+                 * 
+                 */
+                //Add more rules above. Do not change below.
+                triggered = true;
+            }
+        }
+    }
+}
